1547c0eefe671e9731716eff54e284225abdeb66
Berk9626 <berkberkmen12@gmail.com>
2026-01-23 17:24:44 +0300

    added orderapi and some cqrs systems fully

---
+++ Services/Order/Presentation/MutlShop.Order.WebApi/Controllers/AdressesController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Order.Application.Features.CQRS.Commands.AddressCommands;
+using MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers;
+using MultiShop.Order.Application.Features.CQRS.Queries.AddressQueries;
+
+namespace MutlShop.Order.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdressesController : ControllerBase
+    {
+        private readonly GetAdressQueryHandler _getadressqueryHandler;
+        private readonly GetAddressByIdQueryHandler _getaddressByIdQueryHandler;
+        private readonly CreateAddressCommandHandler _createaddressCommandHandler;
+        private readonly UpdateAddressCommandHandler _updateaddressCommandHandler;
+        private readonly RemoveAddressCommandHandler _removeaddressCommandHandler;
+
+        public AdressesController(GetAdressQueryHandler getAdressQueryHandler, GetAddressByIdQueryHandler getAddressByIdQueryHandler, CreateAddressCommandHandler createaddressCommandHandler, UpdateAddressCommandHandler updateaddressCommandHandler, RemoveAddressCommandHandler removeaddressCommandHandler)
+        {
+            _getadressqueryHandler = getAdressQueryHandler;
+            _getaddressByIdQueryHandler = getAddressByIdQueryHandler;
+            _createaddressCommandHandler = createaddressCommandHandler;
+            _updateaddressCommandHandler = updateaddressCommandHandler;
+            _removeaddressCommandHandler = removeaddressCommandHandler;
+        }
+        [HttpGet]
+        public async Task<IActionResult> AddressList()
+        {
+            var values = _getadressqueryHandler.Handle();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> AddressListById(int id)
+        {
+            var values = await _getaddressByIdQueryHandler.Handle(new GetAddressByIdQuery(id));
+            return Ok(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateAddress(CreateAddressCommand command)
+        {
+            await _createaddressCommandHandler.Handle(command);
+            return Ok("Added successfully");
+
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateAddress(UpdateAdressCommand command)
+        {
+            await _updateaddressCommandHandler.Handle(command);
+            return Ok("Updated successfully");
+
+        }
+        [HttpDelete]
+        public async Task<IActionResult> RemoveAddress(int id)
+        {
+            await _removeaddressCommandHandler.Handle(new RemoveAddressCommand(id));
+            return Ok("Removed successfully");
+
+        }
+
+
+    }
+}
---
+++ Services/Order/Presentation/MutlShop.Order.WebApi/Controllers/OrderDetailsController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Order.Application.Features.CQRS.Commands.OrderDetailCommands;
+using MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers;
+using MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers;
+using MultiShop.Order.Application.Features.CQRS.Queries.OrderDetailQueries;
+
+namespace MutlShop.Order.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderDetailsController : ControllerBase
+    {
+        private readonly CreateOrderDetailCommandHandler _createOrderDetailCommandHandler;
+        private readonly GetOrderDetailByIdQueryHandler _getOrderDetailByIdQueryHandler;
+        private readonly GetOrderDetailQueryHandler _getOrderDetailQueryHandler;
+        private readonly RemoveOrderDetailCommandHandler _removeOrderDetailCommandHandler;
+        private readonly UpdateOrderDetailCommandHandler _updateOrderDetailCommandHandler;
+
+        public OrderDetailsController(CreateOrderDetailCommandHandler createOrderDetailCommandHandler, 
+            GetOrderDetailByIdQueryHandler getOrderDetailByIdQueryHandler, GetOrderDetailQueryHandler getOrderDetailQueryHandler, 
+            RemoveOrderDetailCommandHandler removeOrderDetailCommandHandler, UpdateOrderDetailCommandHandler updateOrderDetailCommandHandler)
+        {
+            _createOrderDetailCommandHandler = createOrderDetailCommandHandler;
+            _getOrderDetailByIdQueryHandler = getOrderDetailByIdQueryHandler;
+            _getOrderDetailQueryHandler = getOrderDetailQueryHandler;
+            _removeOrderDetailCommandHandler = removeOrderDetailCommandHandler;
+            _updateOrderDetailCommandHandler = updateOrderDetailCommandHandler;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> OrderDetailList()
+        {
+            var values = _getOrderDetailQueryHandler.Handle();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> OrderDetailListById(int id)
+        {
+            var values = await _getOrderDetailByIdQueryHandler.Handle(new GetOrderDetailByIdQuery(id));
+            return Ok(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateOrderDetail(CreateOrderDetailCommand command)
+        {
+            await _createOrderDetailCommandHandler.Handle(command);
+            return Ok("Added successfully");
+
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateOrderDetail(UpdateOrderDetailCommand command)
+        {
+            await _updateOrderDetailCommandHandler.Handle(command);
+            return Ok("Updated successfully");
+
+        }
+        [HttpDelete]
+        public async Task<IActionResult> RemoveOrderDetail(int id)
+        {
+            await _removeOrderDetailCommandHandler.Handle(new RemoveOrderDetailCommand(id));
+            return Ok("Removed successfully");
+
+        }
+
+    }
+}
