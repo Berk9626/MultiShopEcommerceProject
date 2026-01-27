65f06dff63700c140a77345f07eb4c793145e324
Berk9626 <berkberkmen12@gmail.com>
2026-01-27 04:13:57 +0300

    updated

--- Services/Order/Presentation/MutlShop.Order.WebApi/Program.cs
+++ Services/Order/Presentation/MutlShop.Order.WebApi/Program.cs
@@ -6,7 +6,7 @@ using MultiShop.Order.Persistance.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container..
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 builder.Services.AddApplicationService(builder.Configuration);
 
