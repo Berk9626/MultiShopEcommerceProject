2e424fdb6220337223043c032b701c20b7701c97
Berk9626 <berkberkmen12@gmail.com>
2026-01-29 16:23:09 +0300

    added jsonwebtoken to the Identity4.0(include clients, scopes and resources)

--- IdentityServer/MultiShop.IdentityServer/Config.cs
+++ IdentityServer/MultiShop.IdentityServer/Config.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -9,50 +10,137 @@ namespace MultiShop.IdentityServer
 {
     public static class Config
     {
-        public static IEnumerable<IdentityResource> IdentityResources =>
-                   new IdentityResource[]
-                   {
-                new IdentityResources.OpenId(),
-                new IdentityResources.Profile(),
-                   };
+        //Apiresource çağrıldığı zaman ben her bir microservicem için, o serviceye erişim sağlayabilencek bir key belirleyeceğim.
+        public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
+        {
+            //ResourceCatalog ismine sahip bir mikroservis kullanıcısı, catalog full per. işlemini gerçekleştirecek
 
-        public static IEnumerable<ApiScope> ApiScopes =>
-            new ApiScope[]
+            new ApiResource("ResourceCatalog") 
             {
-                new ApiScope("scope1"),
-                new ApiScope("scope2"),
-            };
-
-        public static IEnumerable<Client> Clients =>
-            new Client[]
+                Scopes={"CatalogFullPermission","CatalogReadPermission"} // aralığı şu
+            },
+            new ApiResource("ResourceDiscount")
             {
-                // m2m client credentials flow client
-                new Client
-                {
-                    ClientId = "m2m.client",
-                    ClientName = "Client Credentials Client",
+                Scopes={"DiscountFullPermission"}
+            },
+            new ApiResource("ResourceOrder")
+            {
+                Scopes={"OrderFullPermission"}
+            }
+        };
 
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+        public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[]
+        {
+            //identity kaynağına sahip olan kişi hangi değerlere erişim sağlayacak ?
 
-                    AllowedScopes = { "scope1" }
-                },
+            new IdentityResources.OpenId(), //herkese açık olan id'ye erişim sağlayacak.
+            new IdentityResources.Email(), //herkese açık olan email'ye erişim sağlayacak.
+            new IdentityResources.Profile(),
 
-                // interactive client using code flow + pkce
-                new Client
-                {
-                    ClientId = "interactive",
-                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+            //IdentityResources ifadesiyle tokenının aldığım kullanıcı o token içerisindeki hangi bilgilere erişim sağlayacağını bildirdim.
+        };
 
-                    AllowedGrantTypes = GrantTypes.Code,
+        public static IEnumerable<ApiScope> ApiScopes => new ApiScope[] //bir yetkiye sahip olduğunda yapacağı işlemler
+        {
+            new ApiScope("CatalogFullPermission","Full authority for catalog operations"), //CatalogFullPermission'a sahip olan kişinin yapabileceği işlemler
+            new ApiScope("CatalogReadPermission","Reading authority for catalog operations"),
+            new ApiScope("DiscountFullPermission","Full authority for discount operations"),
+            new ApiScope("OrderFullPermission","Full authority for order operations")
+        };
 
-                    RedirectUris = { "https://localhost:44300/signin-oidc" },
-                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+        public static IEnumerable<Client> Clients => new Client[]
+        {
+            //visitorın sahip olacağı izinler !!
+            new Client
+            {
+                ClientId="MultiShopVisitorId",
+                ClientName="MultiShop Visitor User",
+                AllowedGrantTypes=GrantTypes.ClientCredentials, //AllowedGrantTypes= neye izin verdiğiyle alakalı
+                ClientSecrets= {new Secret("multishopsecret".Sha256())},
+                AllowedScopes={ "CatalogReadPermission" }
+            },
 
-                    AllowOfflineAccess = true,
-                    AllowedScopes = { "openid", "profile", "scope2" }
-                },
-            };
+            //manager
+            new Client
+            {
+                ClientId="MultiShopManagerId",
+                ClientName="MultiShop Manager User",
+                AllowedGrantTypes=GrantTypes.ClientCredentials, //AllowedGrantTypes= neye izin verdiğiyle alakalı
+                ClientSecrets= {new Secret("multishopsecret".Sha256())},
+                AllowedScopes={ "CatalogReadPermission", "CatalogFullPermission" }
+            },
+
+            //admin
+              new Client
+            {
+                ClientId="MultiShopAdminId",
+                ClientName="MultiShop Admin User",
+                AllowedGrantTypes=GrantTypes.ClientCredentials, //AllowedGrantTypes= neye izin verdiğiyle alakalı
+                ClientSecrets= {new Secret("multishopsecret".Sha256())},
+                AllowedScopes={ "CatalogReadPermission", "CatalogFullPermission", "DiscountFullPermission", "OrderFullPermission",
+                  IdentityServerConstants.LocalApi.ScopeName, //yerelde tuttuğum api üzerinden scope'un yani kapsamın adına ulaş
+                  IdentityServerConstants.StandardScopes.Email, //adminin emailini göster
+                  IdentityServerConstants.StandardScopes.OpenId,
+                  IdentityServerConstants.StandardScopes.Profile
+                  },
+                AccessTokenLifetime=600 // 5 DAKİKA SONRA TOKEN ÖMRÜ SONA ERECEK
+
+              }
+
+
+
+        };
+
+
+
+
+
+
+
+        //public static IEnumerable<IdentityResource> IdentityResources =>
+        //           new IdentityResource[]
+        //           {
+        //        new IdentityResources.OpenId(),
+        //        new IdentityResources.Profile(),
+        //           };
+
+        //public static IEnumerable<ApiScope> ApiScopes =>
+        //    new ApiScope[]
+        //    {
+        //        new ApiScope("scope1"),
+        //        new ApiScope("scope2"),
+        //    };
+
+        //public static IEnumerable<Client> Clients =>
+        //    new Client[]
+        //    {
+        //        // m2m client credentials flow client
+        //        new Client
+        //        {
+        //            ClientId = "m2m.client",
+        //            ClientName = "Client Credentials Client",
+
+        //            AllowedGrantTypes = GrantTypes.ClientCredentials,
+        //            ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+
+        //            AllowedScopes = { "scope1" }
+        //        },
+
+        //        // interactive client using code flow + pkce
+        //        new Client
+        //        {
+        //            ClientId = "interactive",
+        //            ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+
+        //            AllowedGrantTypes = GrantTypes.Code,
+
+        //            RedirectUris = { "https://localhost:44300/signin-oidc" },
+        //            FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
+        //            PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+
+        //            AllowOfflineAccess = true,
+        //            AllowedScopes = { "openid", "profile", "scope2" }
+        //        },
+        //    };
     }
 }
