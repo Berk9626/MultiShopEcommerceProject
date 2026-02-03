fe4a329c63114a1f72d6f10f66b712a19f389d3e
Berk9626 <berkberkmen12@gmail.com>
2026-02-03 17:13:46 +0300

    Added_cargoservicewithrepositorydesignpattern_halfofit

---
+++ Services/Cargo/MultiShop.Cargo.BusinessLayer/Concrete/CargoCompanyManager.cs
@@ -0,0 +1,46 @@
+﻿using MultiShop.Cargo.BusinessLayer.Abstract;
+using MultiShop.Cargo.DataAccessLayer.Abstract;
+using MultiShop.Cargo.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.BusinessLayer.Concrete
+{
+    public class CargoCompanyManager : ICargoCompanyService
+    {
+        private readonly ICargoCompanyDal _CargoCompanyDal;
+
+        public CargoCompanyManager(ICargoCompanyDal CargoCompanyDal)
+        {
+            _CargoCompanyDal = CargoCompanyDal;
+        }
+
+        public void TDelete(int id)
+        {
+            _CargoCompanyDal.Delete(id);
+        }
+
+        public List<CargoCompany> TGetAll()
+        {
+            return _CargoCompanyDal.GetAll();
+        }
+
+        public CargoCompany TGetById(int id)
+        {
+            return _CargoCompanyDal.GetById(id);
+        }
+
+        public void TInsert(CargoCompany entity)
+        {
+            _CargoCompanyDal.Insert(entity);
+        }
+
+        public void TUpdate(CargoCompany entity)
+        {
+            _CargoCompanyDal.Update(entity);
+        }
+    }
+}
---
+++ Services/Cargo/MultiShop.Cargo.WebApi/Controllers/CargoCompanyController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Cargo.BusinessLayer.Abstract;
+using MultiShop.Cargo.DtoLayer.Dtos.CargoCompanyDtos;
+using MultiShop.Cargo.EntityLayer.Concrete;
+
+namespace MultiShop.Cargo.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CargoCompanyController : ControllerBase
+    {
+        private readonly ICargoCompanyService _cargoCompanyService;
+
+        public CargoCompanyController(ICargoCompanyService cargoCompanyService)
+        {
+            _cargoCompanyService = cargoCompanyService;
+        }
+
+        [HttpGet]
+        public IActionResult CargoCompanyList()
+        {
+            var values = _cargoCompanyService.TGetAll();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CreateCargoCompany(CreateCargoCompanyDto createCargoCompanydto)
+        {
+            CargoCompany cargoCompany = new CargoCompany()
+            {
+                CargoCompanyName = createCargoCompanydto.CargoCompanyName,
+            };
+
+            _cargoCompanyService.TInsert(cargoCompany);
+            return Ok("Cargo company added successfully!");
+        }
+        [HttpDelete]
+        public IActionResult RemoveCargoCompany(int id)
+        {
+            _cargoCompanyService.TDelete(id);
+            return Ok("Cargo removed successfully");
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetCargoCompanyById(int id)
+        {
+            var value = _cargoCompanyService.TGetById(id);
+            return Ok("Cargo removed successfully");
+        }
+        [HttpPut]
+        public IActionResult UpdateCargoCompany(UpdateCargoCompanyDto updateCargoCompanyDto)
+        {
+            CargoCompany cargoCompany = new CargoCompany()
+            {
+                CargoCompanyId = updateCargoCompanyDto.CargoCompanyId,
+                CargoCompanyName = updateCargoCompanyDto.CargoCompanyName
+            };
+            _cargoCompanyService.TUpdate(cargoCompany);
+            return Ok("Cargo company updated succesfully");
+        }
+    }
+}
