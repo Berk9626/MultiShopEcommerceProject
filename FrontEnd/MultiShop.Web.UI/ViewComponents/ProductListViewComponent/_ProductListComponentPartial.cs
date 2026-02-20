3816c6c35177e35824189b7137ef1d4d47e2c1d6
Berk9626 <berkberkmen12@gmail.com>
2026-02-20 15:53:14 +0300

    Updated ProductFilter viewcomponents

---
+++ FrontEnd/MultiShop.Web.UI/ViewComponents/ProductListViewComponent/_ProductListComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.Web.UI.ViewComponents.ProductListViewComponent
+{
+    public class _ProductListComponentPartial: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
