818f0adc203af9fc9caedadd00353c1ed5f7f64d
Berk9626 <berkberkmen12@gmail.com>
2026-02-21 10:42:27 +0300

    basicshoppingcard_ended

---
+++ FrontEnd/MultiShop.Web.UI/Controllers/ShoppingCardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.Web.UI.Controllers
+{
+    public class ShoppingCardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ FrontEnd/MultiShop.Web.UI/ViewComponents/ShoppingCardViewComponents/_ShoppingCardDiscountCouponComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.Web.UI.ViewComponents.ShoppingCardViewComponents
+{
+    public class _ShoppingCardDiscountCouponComponentPartial: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
