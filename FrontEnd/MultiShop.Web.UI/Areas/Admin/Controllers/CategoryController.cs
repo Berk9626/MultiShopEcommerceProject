d1df96f64bbf5ac63fd7f66adf31adb7a55bf030
Berk9626 <berkberkmen12@gmail.com>
2026-03-13 17:14:11 +0300

    product_area_added

--- FrontEnd/MultiShop.Web.UI/Areas/Admin/Controllers/CategoryController.cs
+++ FrontEnd/MultiShop.Web.UI/Areas/Admin/Controllers/CategoryController.cs
@@ -76,5 +76,41 @@ namespace MultiShop.Web.UI.Areas.Admin.Controllers
             return View();
 
         }
+
+        [Route("UpdateCategory/{id}")]
+        [HttpGet]
+        public async Task<IActionResult> UpdateCategory(string id)
+        {
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Kategoriler";
+            ViewBag.v3 = " Kategori Girişi";
+            @ViewBag.v0 = "Kategori İşlemleri";
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7070/api/Categories/id?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateCategoryDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [Route("UpdateCategory/{id}")]
+        [HttpPost]
+        public async Task<IActionResult> UpdateCategory(UpdateCategoryDto updateCategoryDto )
+        {
+            
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateCategoryDto);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7070/api/Categories/" , content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Category", new { area = "Admin" });
+            }
+            return View();
+        }
     }
 }
